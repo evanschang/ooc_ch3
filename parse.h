df9ad9efd2392528c34d1a2c2c0e33a21b0d9142
evanschang <a86741472@gmail.com>
2014-08-11 21:57:31 +0800

    ooc ch3

---
+++ parse.h
@@ -0,0 +1,13 @@
+/*
+ * parse.h
+ *
+ *  Created on: 2014¦~7¤ë29¤é
+ *      Author: EvansChang
+ */
+
+#ifndef PARSE_H_
+#define PARSE_H_
+
+
+
+#endif /* PARSE_H_ */
---
+++ value.h
@@ -0,0 +1,18 @@
+/*
+ * value.h
+ *
+ *  Created on: 2014¦~8¤ë1¤é
+ *      Author: Evans
+ */
+
+#ifndef VALUE_H_
+#define VALUE_H_
+
+const void *Add;
+
+void* now (const void *type, ...);
+void process (const void *tree);
+void delete (void *tree);
+
+
+#endif /* VALUE_H_ */
